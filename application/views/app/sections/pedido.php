aca3b4777af29afbeccf65a9411c3f36aaa84004
Alfred Ortega <omar.alfredo49@gmail.com>
2020-08-07 01:21:14 -0500

    Add menú view

--- application/views/app/sections/pedido.php
+++ application/views/app/sections/pedido.php
@@ -1,4 +1,95 @@
 
-            <div class="menu d-none animate__animated">
-              <h3>Menú de la cafeteria</h3>
+            <div class="menu animate__animated">
+
+              <div class="card">
+                <div class="row">
+                  <div class="col-6">
+                    <ul class="list-group list-group-flush">
+                      <li class="list-group-item">Mi carrito</li>
+                    </ul>
+                  </div>
+                  <div class="col-6 text-right">
+                    <ul class="list-group list-group-flush">
+                      <li class="list-group-item">
+                        <a href="#" id="cart-button">
+                          <i class="fas fa-shopping-cart"></i>
+                          <span id="cart-items">0</span>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+
+              <div class="options">
+                <div class="row">
+                  <div class="col-1"></div>
+                  <div class="col-5">
+                    <div class="card option">
+                      <div class="card-body">
+                        <i class="fas fa-3x fa-wine-bottle"></i>
+                        <br>
+                        <p>Bebidas</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-5">
+                    <div class="card option">
+                      <div class="card-body">
+                        <i class="fas fa-3x fa-drumstick-bite"></i>
+                        <br>
+                        <p>Comida</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-1"></div>
+                </div>
+
+                <div class="row">
+                  <div class="col-1"></div>
+                  <div class="col-5">
+                    <div class="card option">
+                      <div class="card-body">
+                        <i class="fas fa-3x fa-candy-cane"></i>
+                        <br>
+                        <p>Dulces</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-5">
+                    <div class="card option">
+                      <div class="card-body">
+                        <i class="fas fa-3x fa-cookie-bite"></i>
+                        <br>
+                        <p>Frituras</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-1"></div>
+                </div>
+
+                <div class="row">
+                  <div class="col-1"></div>
+                  <div class="col-5">
+                    <div class="card option">
+                      <div class="card-body">
+                        <i class="fas fa-3x fa-apple-alt"></i>
+                        <br>
+                        <p>Productos sanos</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-5">
+                    <div class="card option">
+                      <div class="card-body">
+                        <i class="fas fa-3x fa-utensils"></i>
+                        <br>
+                        <p>Menú del dia</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-1"></div>
+                </div>
+              </div>
+
             </div>
